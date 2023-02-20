253e3ff26c81f7f94ed1f3ae958e9639bba974f1
dcd11 <dcduggins115@gmail.com>
2023-02-20 11:50:39 -0500

    new file

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -1,19 +1,3 @@
 public class BigNumArithmetic {
-    public BigNumArithmetic() {
 
-    }
-
-    public static void main(String args[]) {
-        // initate File reader
-        // iterate through the File while(file.hasNext())
-        // read in value from first line
-        // add ints to Linked List
-        // add Linked List to Stack
-        // pop() from the Stack when we reach operand
-        // perform operation
-        // add Linked List int result to the Stack
-        // continue to iterate through the file
-
-        // if the Stack is null, return not a valid equation
-    }
 }
